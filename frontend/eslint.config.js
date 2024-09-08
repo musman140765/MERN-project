c38b74fa75b8a6a8785e10e169648b2c7c4342cd
musman140765 <122722451+musman140765@users.noreply.github.com>
2024-09-08 14:00:03 +0500

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,12 @@
-// eslint.config.js
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {},
+  languageOptions: {
+    globals: {
+      node: true
+    }
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-console": "warn"
+  }
 };
-
